0564248ee513953fa1eba6e87ae3576279903d0b
Nadezhda <nbykova79@gmail.com>
2022-10-02 15:59:55 +0300

    модуль вывода расписания

---
+++ print_all.py
@@ -0,0 +1,9 @@
+def find_all():
+    f=open('schedule.csv', 'r', encoding='UTF-8')
+    all_data=f.read().split('\n')
+    # print(all_data)
+    for i in range(len(all_data)-1):
+        print_all=all_data[i].split(';')
+        print(print_all)
+       
+    f.close()
