1f5301ac681c2a1cdb090549bdf9dbb3e2de60d7
Nadezhda <nbykova79@gmail.com>
2022-10-02 15:56:31 +0300

    модуль ввода расписания

---
+++ input_sched.py
@@ -0,0 +1,15 @@
+def input_numless():
+    num=input('Введите номер урока ')
+    return num
+
+def input_room():
+    num_class=input('Введите номер кабинета: ')
+    return num_class
+
+def input_less():
+    less=input('Введите предмет: ')
+    return less
+
+def input_class():
+    class1=input('Введите класс учеников: ')
+    return class1
