04949529311ca3622f1a664e51c1815f89e34c98
Nadezhda <nbykova79@gmail.com>
2023-02-17 20:48:18 +0300

    Add modul menu for user

--- menu.py
+++ menu.py
@@ -1,32 +1,49 @@
 import print_all as pa
-import find_sched as fsch
-import find_room as fr
+import find_recipe as frcp
 import loggiter as log
+import input_recipe as inprcp
+import change_recipe as chrcp
+import delete_recipe as dlrcp
 
 
 def start():
-    var = input('Добро пожаловать!\nЕсли Вы хотите ввести новую информацию введите 1\nЕсли Вы хотите найти информацию введите 2\n')
+    var = input('Добро пожаловать!\nЕсли Вы хотите ввести новый рецепт введите 1\nЕсли Вы хотите найти рецепт введите 2\nЕсли вы хотите удалить рецепт введите 3\n')
     if var == '2':
-        var2= input('Ведите цифру выбора\n1- Распечатать расписание учителя\n2- Распечатать общее расписание\n3- Вывести какой урок в это время в кабинете\n')
+        var2= input('Ведите цифру выбора\n1- Изменить рецепт\n2- Показать рецепт\n3- Показать все рецепты\n')
         if var2=='1':
-            name=input('Введите имя преподавателя: ')
-            id_name=fsch.find_id(name)
-            print(id_name)
-            if id_name=='':
-                print("Такого преподавателя нет в нашей школе")
+            word=input('Введите название рецепта: ')
+            head=frcp.find_word(word)
+            # print(head)
+            if head=='':
+                print("Такого рецепта нет")
             else:
-                fsch.find_data(id_name,name)
-        if var2=='2':
-            pa.find_all()
+                recipe=input('Введите новый рецепт: ')
+                chrcp.change_recipe(head, recipe)
+                print('Рецепт изменен')
         if var2=='3':
-            less=input("Введите номер урока:\n1: 9-00 - 9-45\n2: 10-00 - 10-45\n3: 11-00 - 11-45\n4: 12-00 - 12-45\n")
-            room=input('Введите номер кабинета\n')
-            fr.find_room(less,room)
-
+            pa.find_all()
+        if var2=='2':
+            var3=input('Поиск по дате - введите 1\nПоиск по названию - нажмите 2\n')
+            if var3=='1':
+                date=input('Введите дату ')
+                # output=frcp.find_date(date)
+                # print(output)
+                frcp.find_date(date)
+            if var3=='2':
+                word=input('Введите название ')
+                frcp.find_head(word)
+                # print(output) 
     if var == '1':
-        var2= input('Что Вы хотите сделать?\n1- Ввести информацию про нового учителя\n2- Ввести информацию в расписание\n')
-        if var2=='1':
-            log.log_teach()
-        if var == '2':
-            log.log_sched()
+        log.log_recipe()
+        print('Новый рецепт добавлен')
+    if var == '3':
+        word=input('Введите название рецепта: ')
+        head=frcp.find_word(word)
+            # print(head)
+        if head=='':
+            print("Такого рецепта нет")
+        else:
+            dlrcp.delete_recipe(head)
+            print('Рецепт удален')
+        
        
